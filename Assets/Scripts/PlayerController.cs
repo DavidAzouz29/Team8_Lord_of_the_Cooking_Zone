47668dfa31072c6cce72861007ca0faa8327e0ee
David Azouz <david.azouz@ad.aie.edu>
2016-06-21 14:37:54 +1000

    + Player Controller added again :/

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@
 /// ----------------------------------
 /// Brief: Player Controller class that controls the player.
 /// viewed: https://unity3d.com/learn/tutorials/projects/roll-a-ball/moving-the-player
-/// 
+/// http://wiki.unity3d.com/index.php?title=Xbox360Controller
 /// *Edit*
 /// - Player state machine - 20/06/2016
 /// TODO:
@@ -31,10 +31,12 @@ public class PlayerController : MonoBehaviour
     [Tooltip("This will change at runtime.")]
     public string verticalAxis = "Vertical";
     public string horizontalAxis = "Horizontal";
+    public string rotationAxisX = "Rotation_X";
+    public string rotationAxisY = "Rotation_X";
 
     [Header("Weapon")]
     public GameObject r_weapon;
-    //public GameObject r_gameOverPanel;
+    //public GameObject r_gameOverPanel; //w
     //public GameObject r_bombExplosionParticleEffect;
     //public Camera r_camera;
 
@@ -60,33 +62,18 @@ public class PlayerController : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-<<<<<<< HEAD
-        /*m_rigidBody = GetComponent<Rigidbody>();
-
-        //if rigid body == null
-        if (!m_rigidBody)
-        {
-            Debug.LogError("No Rigidbody");
-        } */     
-
-        //setting our current state to alive
-        m_eCurrentPlayerState = E_PLAYER_STATE.E_PLAYER_STATE_ALIVE;
-
-        /*int randSelection = (int)Random.Range(0, MAX_PLAYERS - 1);
-        Debug.Log("Player: " + randSelection); */
-
-=======
         //setting our current state to alive
         m_eCurrentPlayerState = E_PLAYER_STATE.E_PLAYER_STATE_ALIVE;
 
         // Loops through our players and assigns variables for input from different controllers
->>>>>>> 4ae7ac31804f9c8c43fc87f6d3cc52ad44bf9b18
         for (uint i = 0; i < MAX_PLAYERS; ++i)
         {
             if (m_playerID == i)
             {
                 verticalAxis = "P" + (i + 1) + "_Vertical";
                 horizontalAxis = "P" + (i + 1) + "_Horizontal";
+                rotationAxisX = "P" + (i + 1) + "_Rotation_X";
+                rotationAxisY = "P" + (i + 1) + "_Rotation_Y";
             }
         }        
     }
@@ -97,12 +84,9 @@ public class PlayerController : MonoBehaviour
         //creating a variable that gets the input axis
         float moveHorizontal = Input.GetAxis(horizontalAxis);
         float moveVertical = Input.GetAxis(verticalAxis);
+        float moveRotationX = Input.GetAxis(rotationAxisX);
+        float moveRotationY = Input.GetAxis(rotationAxisY);
 
-<<<<<<< HEAD
-        Vector3 movementDirection = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        Vector3 pos = transform.position + movementDirection * playerSpeed * Time.deltaTime;
-        transform.position = Vector3.Lerp(transform.position, pos, 0.2f);
-=======
         // Movement
         if (moveHorizontal < -fRot || moveHorizontal > fRot ||
             moveVertical < -fRot || moveVertical > fRot)
@@ -127,9 +111,9 @@ public class PlayerController : MonoBehaviour
         }
 
         //m_rigidBody.AddForce(movementDirection * playerSpeed * Time.deltaTime);
->>>>>>> 4ae7ac31804f9c8c43fc87f6d3cc52ad44bf9b18
 
         //Switches between player states
+        #region Player States
         switch (m_eCurrentPlayerState)
         {
             //checks if the player is alive
@@ -162,21 +146,7 @@ public class PlayerController : MonoBehaviour
                     break;
                 }
         }
-<<<<<<< HEAD
-
-        // if we topple over
-        if(Input.GetButton("Reset"))
-        {
-            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.identity, 0.2f);
-        }
-        //TODO: change to random number
-        if(m_playerID == 2 && Time.deltaTime >= 60)
-        {
-            m_eCurrentPlayerState = E_PLAYER_STATE.E_PLAYER_STATE_DEAD;
-        }
-=======
         #endregion
->>>>>>> 4ae7ac31804f9c8c43fc87f6d3cc52ad44bf9b18
 	}
 
     void BombEffectDead()
@@ -219,6 +189,14 @@ public class PlayerController : MonoBehaviour
     {
         m_playerID = a_uiPlayerID; 
     }
+
+    // Upon Collision TODO: is this still needed?
+    /*void OnCollisionEnter()
+    {
+        Vector3 v3PreviousPos = transform.localPosition;
+        transform.parent.position = transform.localPosition;
+        transform.position = v3PreviousPos;
+    } */
 
     /*void OnCollisionStay(Collision a_collision)
     {
