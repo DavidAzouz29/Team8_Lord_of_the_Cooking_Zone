99bf4fa935da0596d790fa816cb897700c5e8599
Daniel Spiteri <daniel.spiteri@ad.aie.edu>
2016-06-22 11:21:58 +1000

    +Textures +Throw mechanic balancing

---
+++ Assets/Scripts/WinState.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+ 
+
+public class WinState : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Play(){
+
+		SceneManager.LoadScene (1);
+	}
+
+
+	public void MainMenu(){
+
+		SceneManager.LoadScene (0);
+
+	}
+
+	public void Quit(){
+
+		Application.Quit();
+	}
+}
